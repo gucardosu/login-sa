da688ae44eed07891bb05e806c11f1ba7770e1f5
GUSTAVO JOÃO CARDOSO <gustavo_j_cardoso@estudante.sesisenai.org.br>
2026-04-30 14:29:20 -0300

    feat: auth.routes atualizado

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { UserRegister } from "../controllers/auth.controller.js";
+import { UserLogin } from "../controllers/auth.controller.js";
 
 const authRouter = Router();
 
-authRouter.post('/cadastro', UserRegister)
+authRouter.post("/cadastro", UserRegister);
+authRouter.post("/login", UserLogin);
 
-
-export default authRouter
+export default authRouter;
