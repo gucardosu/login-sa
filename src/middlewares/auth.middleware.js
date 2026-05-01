0eaf74e9b5beed081eb64c1629ddc4f0ceb3c26a
gucardosu <gustavocardosodot@gmail.com>
2026-05-01 16:07:37 -0300

    feat: correção de erros do auth.middleware

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,19 +3,18 @@ import jwt from "jsonwebtoken";
 export function auth(req, res, next) {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || authHeader.startsWith("Bearer ")) {
-    return res.status(401).json({ error: "Token ausente ou mal formatado. " });
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ error: "Token ausente ou mal formatado." });
   }
 
   const token = authHeader.split(" ")[1];
 
   try {
-    const payloadDecoder = jwt.verify(token, process.env.JWT_SECRET);
+    const payloadDecodificado = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.usuario = payloadDecoder;
-
+    req.usuario = payloadDecodificado;
     next();
   } catch (error) {
-    return res.status(401).json({ error: "Token inválido ou expirado!" });
+    return res.status(401).json({ error: "Token inválido ou expirado" });
   }
 }
