4a9609e23798ee84b257c6bc9761f67e465946ea
gucardosu <gustavocardosodot@gmail.com>
2026-05-01 15:27:13 -0300

    feat: importação de rotas no app.js

--- src/app.js
+++ src/app.js
@@ -1,16 +1,18 @@
-import 'dotenv/config';
-import express from 'express';
-import sequelize from './config/database.js';
-import './models/usuario.model.js';
-import authRouter from './routes/auth.routes.js'
+import "dotenv/config";
+import express from "express";
+import sequelize from "./config/database.js";
+import "./models/usuario.model.js";
+import authRouter from "./routes/auth.routes.js";
+import usuarioRouter from "./routes/usuario.routes.js";
 
 const app = express();
 app.use(express.json());
 
-app.use('/auth', authRouter)
+app.use("/auth", authRouter);
+app.use("/usuario", usuarioRouter);
 
 sequelize.sync({ alter: true }).then(() => {
   app.listen(process.env.PORT, () =>
-    console.log(`Servidor rodando na porta ${process.env.PORT}`)
+    console.log(`Servidor rodando na porta ${process.env.PORT}`),
   );
 });
